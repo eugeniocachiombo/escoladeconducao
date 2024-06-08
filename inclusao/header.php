538616ba84e0aa3917ba092fd436dddebc01c8e4
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-06-08 16:44:19 +0100

    Página inicio criado

--- inclusao/header.php
+++ inclusao/header.php
@@ -1,87 +1,95 @@
 <style>
-         header {
-            background-color: cadetblue;
-            min-height: 20vh;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 20px;
-        }
-        .logotipo {
-            font-size: 24px;
-            font-weight: bold;
-            color: white;
-            text-decoration: none;
-        }
-        nav ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-        nav ul li {
-            margin-right: 20px;
-            position: relative;
-        }
-        nav ul li a {
-            color: white;
-            text-decoration: none;
-            padding: 10px;
-            display: block;
-        }
-        nav ul li ul {
-            display: none;
-            position: absolute;
-            background-color: cadetblue;
-            min-width: 120px;
-            z-index: 1;
-        }
-        nav ul li:hover ul {
-            display: block;
-        }
-        nav ul li ul li {
-            margin: 0;
-        }
+    header {
+        background-color: cadetblue;
+        min-height: 20vh;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px;
+    }
 
-        nav ul li:hover a {
-            background-color: darkcyan;
-        }
-        nav ul li ul li:hover a {
-            background-color: darkslategray;
-        }
-    </style>
+    .logotipo {
+        font-size: 24px;
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+    }
 
- <header>
-        <a href="#" class="logotipo" style="font-size: 35px;">
-            <?php 
-                include "../inclusao/logo.php";
-            ?>
-        </a>
-        <nav>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li>
-                    <a href="#">Aluno</a>
-                    <ul>
-                        <li><a href="#">Cadastrar</a></li>
-                        <li><a href="#">Listar</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Instrutor</a>
-                    <ul>
-                        <li><a href="#">Cadastrar</a></li>
-                        <li><a href="#">Listar</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Veículo</a>
-                    <ul>
-                        <li><a href="#">Cadastrar</a></li>
-                        <li><a href="#">Listar</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Sobre</a></li>
-            </ul>
-        </nav>
-    </header>
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+
+    nav ul li {
+        margin-right: 20px;
+        position: relative;
+    }
+
+    nav ul li a {
+        color: white;
+        text-decoration: none;
+        padding: 10px;
+        display: block;
+    }
+
+    nav ul li ul {
+        display: none;
+        position: absolute;
+        background-color: cadetblue;
+        min-width: 120px;
+        z-index: 1;
+    }
+
+    nav ul li:hover ul {
+        display: block;
+    }
+
+    nav ul li ul li {
+        margin: 0;
+    }
+
+    nav ul li:hover a {
+        background-color: darkcyan;
+    }
+
+    nav ul li ul li:hover a {
+        background-color: darkslategray;
+    }
+</style>
+
+<header>
+    <a href="#" class="logotipo" style="font-size: 35px;">
+        <?php
+        include "../inclusao/logo.php";
+        ?>
+    </a>
+    <nav>
+        <ul>
+            <li><a href="../inicio/">Inicio</a></li>
+            <li>
+                <a href="#">Aluno</a>
+                <ul>
+                    <li><a href="../aluno/form-cadastro.php">Cadastrar</a></li>
+                    <li><a href="../aluno">Listar</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Instrutor</a>
+                <ul>
+                    <li><a href="../instrutor/form-cadastro.php">Cadastrar</a></li>
+                    <li><a href="../instrutor">Listar</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Veículo</a>
+                <ul>
+                    <li><a href="../veiculo/form-cadastro.php">Cadastrar</a></li>
+                    <li><a href="../veiculo">Listar</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Sobre</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ inicio/index.php
@@ -0,0 +1,19 @@
+<?php include "../inclusao/head.php"; ?>
+<?php include "../inclusao/header.php"; ?>
+<link rel="stylesheet" href="../assets/css/inputs.css">
+
+<main style="display: flex; justify-content: center;">
+
+    <div style="display: flex; align-items: center; justify-content: center; font-weight: bold;">
+        <h1 style="font-family: TH Charmonman; font-weight: bold; font-size: 50px;">Seja Bem-vindo</h1>
+    </div>
+
+    <div class="form-login" style="display: flex; align-items: center; justify-content: center; ">
+        <div style="font-size: 30vh; ">
+            <i class="fas fa-car" style="color: gray;"></i>
+        </div>
+    </div>
+</main>
+
+<?php include "../inclusao/footer.php"; ?>
+<?php include "../inclusao/foot.php"; ?>

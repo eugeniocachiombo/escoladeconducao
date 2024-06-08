e6742908dcc5c98bd76fc30f13230157b63a3622
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-06-08 16:27:30 +0100

    Menu criado com sucesso

--- inclusao/header.php
+++ inclusao/header.php
@@ -1,3 +1,87 @@
-<header>
-    <h1>Header</h1>
-</header>
+<style>
+         header {
+            background-color: cadetblue;
+            min-height: 20vh;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 20px;
+        }
+        .logotipo {
+            font-size: 24px;
+            font-weight: bold;
+            color: white;
+            text-decoration: none;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+        }
+        nav ul li {
+            margin-right: 20px;
+            position: relative;
+        }
+        nav ul li a {
+            color: white;
+            text-decoration: none;
+            padding: 10px;
+            display: block;
+        }
+        nav ul li ul {
+            display: none;
+            position: absolute;
+            background-color: cadetblue;
+            min-width: 120px;
+            z-index: 1;
+        }
+        nav ul li:hover ul {
+            display: block;
+        }
+        nav ul li ul li {
+            margin: 0;
+        }
+
+        nav ul li:hover a {
+            background-color: darkcyan;
+        }
+        nav ul li ul li:hover a {
+            background-color: darkslategray;
+        }
+    </style>
+
+ <header>
+        <a href="#" class="logotipo" style="font-size: 35px;">
+            <?php 
+                include "../inclusao/logo.php";
+            ?>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li>
+                    <a href="#">Aluno</a>
+                    <ul>
+                        <li><a href="#">Cadastrar</a></li>
+                        <li><a href="#">Listar</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Instrutor</a>
+                    <ul>
+                        <li><a href="#">Cadastrar</a></li>
+                        <li><a href="#">Listar</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Veículo</a>
+                    <ul>
+                        <li><a href="#">Cadastrar</a></li>
+                        <li><a href="#">Listar</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Sobre</a></li>
+            </ul>
+        </nav>
+    </header>

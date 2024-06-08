7c2778a68477f043c303b5d02d38855e5f0798c2
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-06-08 16:03:01 +0100

    Aluno Terminado

---
+++ assets/js/validar-campos-utilizador-editar.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector('form');
+
+    form.addEventListener('submit', function(event) {
+        var nome = document.querySelector('input[name="nome"]');
+        var dataNascimento = document.querySelector('input[name="data_nascimento"]');
+        var genero = document.querySelector('select[name="genero"]');
+        var email = document.querySelector('input[name="email"]');
+        
+        var numeroTelefone = document.querySelector('input[name="numero_de_telefone"]');
+        var numeroSecundario = document.querySelector('input[name="numero_secundario"]');
+        
+        var camposVazios = [];
+
+        if (!nome.value) {
+            camposVazios.push(nome);
+        }
+        if (!dataNascimento.value) {
+            camposVazios.push(dataNascimento);
+        }
+        if (!genero.value) {
+            camposVazios.push(genero);
+        }
+        if (!email.value) {
+            camposVazios.push(email);
+        }
+        if (!numeroTelefone.value) {
+            camposVazios.push(numeroTelefone);
+        }
+        if (!numeroSecundario.value) {
+            camposVazios.push(numeroSecundario);
+        }
+
+       
+        camposVazios.forEach(function(campo) {
+            campo.style.border = '2px solid red';
+        });
+
+        if (camposVazios.length > 0) {
+            event.preventDefault();
+            alert("Por favor, preencha todos os campos.");
+        }
+    });
+
+   
+    form.addEventListener('input', function(event) {
+        event.target.style.border = '';
+    });
+});
